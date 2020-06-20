a152b6e8983cc318e0358dcdf06a15ba8acd26b8
Sumit Sharma <summitkumarsharma@gmail.com>
2020-06-20 17:05:40 +0530

    Initial commit

---
+++ mac/shop/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from .import views
+
+urlpatterns = [
+  path("",views.index, name="shopHome"),
+  path("about/",views.about, name="About Us"),
+  path("services",views.services, name="services"),
+  path("contact",views.contact, name="Contact us"),
+  path("tracker",views.tracker, name="Tracker"),
+  path("productview",views.productview, name="ProductView"),
+  path("search",views.search, name="Search"),
+  path("checkout",views.checkout, name="Checkout")
+]
---
+++ mac/shop/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    # return HttpResponse('shop home')
+    return render(request,'shop/index.html')
+
+def about(request):
+    # return HttpResponse('about shop home')
+    return render(request,'shop/about.html')
+
+def services(request):
+    return HttpResponse('services shop home')
+    # return render(request,'shop/services.html')
+
+def contact(request):
+    return HttpResponse('contact shop home')
+    # return render(request,'shop/contact.html')
+
+def tracker(request):
+    return HttpResponse('tracker shop home')
+    # return render(request,'shop/tracker.html')
+
+
+def productview(request):
+    return HttpResponse('product view shop home')
+    # return render(request,'shop/productview.html')
+
+def search(request):
+    return HttpResponse('search shop home')
+    # return render(request,'shop/search.html')
+
+def checkout(request):
+    return HttpResponse('checkout shop home')
+    # return render(request,'shop/checkout.html')
